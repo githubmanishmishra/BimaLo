c00308571546291ca5e58a4b7cfc355b2f937c6c
Manish <mishramanish677@gmail.com>
2023-06-27 12:26:43 +0530

    Initial commit

---
+++ app/src/main/java/com/laxmi/bimalo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.laxmi.bimalo;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnClick = findViewById(R.id.btn_click);
+        PrefManager prefManager = new PrefManager(getApplicationContext());
+        if(prefManager.isFirstTimeLaunch()){
+            prefManager.setFirstTimeLaunch(false);
+            startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+            finish();
+        }
+    }
+    protected void btn_Click(View view)
+    {
+        Toast.makeText(MainActivity.this, "clicked on button", Toast.LENGTH_LONG).show();
+
+    }
+}
